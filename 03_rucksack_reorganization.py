489d2a1bd0a914821806cf947ae4ab8f98aa3b2c
Paul Vincent <vince8000@gmail.com>
2022-12-03 08:56:30 +0100

    sets up files to start solving

---
+++ 03_rucksack_reorganization.py
@@ -0,0 +1,17 @@
+# Split the strings in half
+# Find unique characters - sets
+# Find common character in each set
+# Test for upper or lower case
+# Score the character and sum
+
+print(ord('a'))
+
+print('B'.isupper())
+
+import string
+
+for c in string.ascii_lowercase:
+    print(f'ASCII for {c} is {ord(c)-96}')
+
+for c in string.ascii_uppercase:
+    print(f'ASCII for {c} is {ord(c)-13}')
