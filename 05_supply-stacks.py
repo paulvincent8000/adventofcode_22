5aef9bd8fdef4f72f5f4371b5dcd5b620e1aa61d
Paul Vincent <vince8000@gmail.com>
2022-12-05 09:51:31 +0000

    gets number of stacks

--- 05_supply-stacks.py
+++ 05_supply-stacks.py
@@ -1,6 +1,23 @@
+import functions as fn
+
 # get input for stacks
+stack_data = 'inputa.txt'
+#stack_data = 'input_05a.txt'
+stack_lines = fn.Reader(stack_data).get_lines()
+
+# find the number of stacks
+def get_number_of_stacks(lines) -> int:
+    rows = []
+    for line in lines:
+        rows.append(line)
+    return rows[-1].count('   ') + 1
+print(get_number_of_stacks(stack_lines))
+
+# build stacks as lists
+
+
+
 # get input for moves
 # parse moves
-# build stacks as lists
 # execute move
 # find top container in stack
