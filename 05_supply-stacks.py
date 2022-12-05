ca811d86ab45c4968b1d9fa828764f3dabd35830
Paul Vincent <vince8000@gmail.com>
2022-12-05 22:34:23 +0000

    builds starting stacks

--- 05_supply-stacks.py
+++ 05_supply-stacks.py
@@ -10,12 +10,48 @@ def get_number_of_stacks(lines) -> int:
     rows = []
     for line in lines:
         rows.append(line)
+        #print(line, len(line))
     return rows[-1].count('   ') + 1
-print(get_number_of_stacks(stack_lines))
 
-# build stacks as lists
+def build_empty_stacks(number_of_stacks) -> list:
+    stacks = []
+    for i in range(number_of_stacks):
+        stacks.append([])
+    return stacks
+
+def stack_crate(stacks, stack, crate) -> None:
+    stacks[stack].append(crate)
+    return
+
+def parse_stack_lines(line) -> list:
+    i = []
+    for stack in range(1, len(line), 4):
+        i.append(line[stack])
+    return i
 
 
+# build stacks as list
+#print(stack_lines[2], parse_stack_lines(stack_lines[2]))
+
+stacks = build_empty_stacks(get_number_of_stacks(stack_lines))
+
+# Fill the stacks
+for line in stack_lines:
+    if line[1] == '1':
+        continue
+    parsed_line = parse_stack_lines(line)
+
+    for stack in range(get_number_of_stacks(stack_lines)):
+        if parsed_line[stack] == ' ':
+            continue
+        stack_crate(stacks, stack, parsed_line[stack])
+        #print(stack, parsed_line[stack], parsed_line[stack] == ' ')
+
+for stack in stacks:
+    stack.reverse()
+
+# print(stacks)
+        
 
 # get input for moves
 # parse moves
