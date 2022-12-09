022c0f765d9d679005d6b16cfc73b50663f4e4f2
Paul Vincent <vince8000@gmail.com>
2022-12-09 21:36:55 +0100

    prepares to handle multiple knots

--- 09_rope_bridge.py
+++ 09_rope_bridge.py
@@ -1,19 +1,45 @@
 import functions as fn
 
-#data = 'input.txt'
-data = 'input_09.txt'
+data = 'input.txt'
+#data = 'input_09.txt'
 
 steps = fn.Reader(data).get_lines()
 
 #print(steps)
 
-# setup vectors
+# setup variables
 xhead = 0
 yhead = 0
 xtail = 0
 ytail = 0
 
 tail_track = []
+head_track = []
+
+class Tail():
+
+    def __init__(self) -> None:
+        self.xtail = 0
+        self.ytail = 0
+        self.trail = []
+
+    def move_tail(self, coordinates:tuple) -> tuple:
+        xhead = coordinates[0]
+        yhead = coordinates[1]
+
+        if abs(yhead - self.ytail) == 2:
+            self.xtail += int(xhead - self.xtail)
+            self.ytail += int((yhead - self.ytail)/2)
+        elif abs(xhead - self.xtail) == 2:
+            self.xtail += int((xhead - self.xtail)/2)
+            self.ytail += int(yhead - self.ytail)
+
+        self.trail.append( (self.xtail,self.ytail) )
+
+        return ( self.xtail , self.ytail )
+
+    def __str__(self) -> str:
+        return f'Unique locations visited: {len(set(self.trail))}'
 
 # parse step and build instructions for head
 def build_head_instructions(step:str) -> list:
@@ -52,6 +78,9 @@ def move_tail() -> None:
 def track_tail() -> None:
     tail_track.append( (xtail,ytail) )
 
+def track_head() -> None:
+    head_track.append( (xhead,yhead) )
+
 for step in steps:
     #print(f'{step}, {build_head_instructions(step)}')
 
@@ -84,3 +113,21 @@ print(f'The tail visited {len(coordinates_visited)} positions.')
 
 # remove duplicate coordinates
 # count coordinates visited
+
+# ====================================================
+xhead = 0
+yhead = 0
+
+tail = Tail()
+
+for step in steps:
+    moves = build_head_instructions(step)
+
+    for move in moves:
+        move_head(move)
+#    print(step, xhead, yhead)  # test move coordinates
+
+        tail.move_tail((xhead,yhead))
+#        print((xhead,yhead),tail.move_tail((xhead,yhead)))
+
+print(tail)
